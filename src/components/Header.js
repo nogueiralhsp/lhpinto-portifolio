4f2584a9b752f114d5fd99011dc0c7f149202df5
Henrique Nogueira <henriquenogueira@henriques-mbp.home>
2021-04-11 04:16:10 +0100

    scss on mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+import { FaBars } from 'react-icons/fa'
+
 const Header = () => (
      <header className="header">
 
           <div className="navigation-menu">
+               <div className="menu-home-active">
+                    <FaBars />
+               </div>
                <ul className="mobile-menu">
-                    <li><NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink></li>
-                    <li><NavLink to="/skills" activeClassName="is-active">Skills</NavLink></li>
-                    <li><NavLink to="/about" activeClassName="is-active">About</NavLink></li>
-                    <li><NavLink to="/contact" activeClassName="is-active">Contact</NavLink></li>
+                    <li><NavLink to="/" className="mobile-menu-link" exact={true}>Home</NavLink></li>
+                    <li><NavLink to="/skills" className="mobile-menu-link">Skills</NavLink></li>
+                    <li><NavLink to="/about" className="mobile-menu-link">About</NavLink></li>
+                    <li><NavLink to="/contact" className="mobile-menu-link">Contact</NavLink></li>
+
                </ul>
+
           </div>
+
           <div className="header-name">
                <h2>Luiz Henrique Pinto</h2>
                <a href="/contact"><p>nogueiralhsp@gmail.com</p></a>
